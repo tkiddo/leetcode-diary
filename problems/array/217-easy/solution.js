98a9c203e7efcac7ef8d5209df179bce1e579447
tkiddo <justforgit@163.com>
2022-11-14 13:03:48 +0800

    problem:217 update

--- problems/array/217-easy/solution.js
+++ problems/array/217-easy/solution.js
@@ -2,7 +2,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const containsDuplicate = function (nums) {}
+const containsDuplicate = function (nums) {
+  const numsMap = {}
+  let result = false
+  for (let i = 0; i < nums.length; i++) {
+    if (numsMap[nums[i]]) {
+      result = true
+      break
+    } else {
+      numsMap[nums[i]] = 1
+    }
+  }
+  return result
+}
 
 module.exports = {
   containsDuplicate,
