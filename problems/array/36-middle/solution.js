447893e43a111251e478dd4753bb6ee7c63aa3c5
tangkaiqiang <justforgit@163.com>
2022-11-22 21:55:24 +0800

    problem: 36 not fixed

--- problems/array/36-middle/solution.js
+++ problems/array/36-middle/solution.js
@@ -2,4 +2,42 @@
  * @param {character[][]} board
  * @return {boolean}
  */
-var isValidSudoku = function (board) {}
+const isValidSudoku = function (board) {
+  const isRepeat = (arr) => {
+    let result = true
+    const hash = {}
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== '.' && hash[arr[i]]) {
+        result = false
+        break
+      } else {
+        result[arr[i]] = true
+      }
+    }
+  }
+  let val = true
+  for (let i = 0; i < board.length; i++) {
+    const rowRepeat = isRepeat(board[i])
+    if (!rowRepeat) {
+      val = false
+      break
+    }
+  }
+  const hash = {}
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board.length; j++) {
+      if (hash[board[j][i]] && board[j][i] !== '.') {
+        val = false
+        break
+      } else {
+        hash[board[j][i]] = true
+      }
+    }
+  }
+
+  return val
+}
+
+module.exports = {
+  isValidSudoku
+}
