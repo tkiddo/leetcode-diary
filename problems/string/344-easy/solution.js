1c787ddbb9a69860c75576ea5d5c939bc8f6935d
tkiddo <justforgit@163.com>
2022-11-24 09:31:52 +0800

    problem: 344 solution

--- problems/string/344-easy/solution.js
+++ problems/string/344-easy/solution.js
@@ -1,8 +1,19 @@
 /**
+ * 我的题解：
+ * 前后位置对调，temp保存临时变量
+ */
+
+/**
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-const reverseString = function (s) {}
+const reverseString = function (s) {
+  for (let i = 0; i < s.length / 2; i++) {
+    let temp = s[i]
+    s[i] = s[s.length - i - 1]
+    s[s.length - i - 1] = temp
+  }
+}
 
 module.exports = {
   reverseString,
