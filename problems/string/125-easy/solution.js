c7b9c53a6083fc0528758b32ff1badda81c39b6e
tkiddo <justforgit@163.com>
2022-11-25 16:57:30 +0800

    problem: 125

--- problems/string/125-easy/solution.js
+++ problems/string/125-easy/solution.js
@@ -2,7 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-const isPalindrome = function (s) {}
+const chars = 'abcdefghijklmnopqrstuvwxyz'
+const isPalindrome = function (s) {
+  const strArr = s.toLocaleLowerCase().split('')
+  const resultArr = []
+  for (let i = 0; i < strArr.length; i++) {
+    if (chars.includes(strArr[i])) {
+      resultArr.push(strArr[i])
+    }
+  }
+  for (let i = 0; i < resultArr.length - i - 1; i++) {
+    if (resultArr[i] !== resultArr[resultArr.length - i - 1]) {
+      return false
+    }
+  }
+  return true
+}
 
 module.exports = {
   isPalindrome,
