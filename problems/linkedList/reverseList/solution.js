f8d8474783fc6d2074cadb2731d4bc59e0399406
tangkaiqiang <justforgit@163.com>
2022-12-03 14:44:07 +0800

    problem: reverseList solution

--- problems/linkedList/reverseList/solution.js
+++ problems/linkedList/reverseList/solution.js
@@ -8,7 +8,31 @@ function ListNode(val, next) {
  * @param {ListNode} head
  * @return {ListNode}
  */
-const reverseList = function (head) {}
+const reverseList = function (head) {
+  if (!head) {
+    return head
+  }
+  if (!head.next) {
+    return head
+  }
+  // 保存反转后当前头节点
+  let currentHead = head
+  // 保存遍历的节点
+  let current = head.next
+  // 反转完成后为最后一个节点，即下一个节点为null，否则会成环
+  currentHead.next = null
+  while (current) {
+    // 保存临时下一个节点
+    let temp = current.next
+    // 掉头
+    current.next = currentHead
+    // 保存新的头节点
+    currentHead = current
+    // 遍历下一个节点
+    current = temp
+  }
+  return currentHead
+}
 
 module.exports = {
   ListNode,
