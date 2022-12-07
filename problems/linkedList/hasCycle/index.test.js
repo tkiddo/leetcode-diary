e9f3c0398c3001d4d5fc9df293c3f288c2718087
tkiddo <justforgit@163.com>
2022-12-07 09:41:18 +0800

    problem: hasCycle test

--- problems/linkedList/hasCycle/index.test.js
+++ problems/linkedList/hasCycle/index.test.js
@@ -1,1 +1,27 @@
-describe.skip('test', () => {})
+const {ListNode,hasCycle} = require('./solution')
+
+describe('hasCycle', () => {
+  it('should return true if the list has a cycle', () => {
+    const head = new ListNode(1)
+    head.next = new ListNode(2)
+    head.next.next = new ListNode(3)
+    head.next.next.next = new ListNode(4)
+    head.next.next.next.next = new ListNode(5)
+    head.next.next.next.next.next = new ListNode(6)
+    head.next.next.next.next.next.next = head.next.next
+
+    expect(hasCycle(head)).toBe(true)
+  })
+
+  it('should return false if the list does not have a cycle', () => {
+    const head = new ListNode(1)
+    head.next = new ListNode(2)
+    head.next.next = new ListNode(3)
+    head.next.next.next = new ListNode(4)
+    head.next.next.next.next = new ListNode(5)
+    head.next.next.next.next.next = new ListNode(6)
+
+    expect(hasCycle(head)).toBe(false)
+  })
+})
+
--- problems/linkedList/hasCycle/solution.js
+++ problems/linkedList/hasCycle/solution.js
@@ -6,8 +6,20 @@
  * }
  */
 
+  function ListNode(val) {
+      this.val = val;
+      this.next = null;
+  }
+
 /**
  * @param {ListNode} head
  * @return {boolean}
  */
-var hasCycle = function (head) {}
+const hasCycle = function (head) {
+  
+}
+
+module.exports = {
+  ListNode,
+  hasCycle
+}
