994498b6a48a98528a4af06bf51dfc71459bf455
tkiddo <justforgit@163.com>
2022-12-12 16:08:08 +0800

    bubbleSort

---
+++ problems/sort/sorter/index.test.js
@@ -0,0 +1,8 @@
+const { bubbleSort } = require('./solution')
+
+describe('bubbleSort', () => {
+  it('should return sorted array', () => {
+    const nums = [2, 3, 0, 4, 1, 6, 9, 10, 8, 7, 5]
+    expect(bubbleSort(nums)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  })
+})
---
+++ problems/sort/sorter/solution.js
@@ -0,0 +1,16 @@
+function bubbleSort(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length - i; j++) {
+      if (nums[j] > nums[j + 1]) {
+        const temp = nums[j]
+        nums[j] = nums[j + 1]
+        nums[j + 1] = temp
+      }
+    }
+  }
+  return nums
+}
+
+module.exports = {
+  bubbleSort,
+}
