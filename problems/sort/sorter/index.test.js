d451d388cd670e35a2b7cb17a5cec19e0041c622
tkiddo <justforgit@163.com>
2022-12-13 09:21:51 +0800

    problem:sort selectionSort

--- problems/sort/sorter/index.test.js
+++ problems/sort/sorter/index.test.js
@@ -1,8 +1,15 @@
-const { bubbleSort } = require('./solution')
+const { bubbleSort, selectionSort } = require('./solution')
 
 describe('bubbleSort', () => {
   it('should return sorted array', () => {
     const nums = [2, 3, 0, 4, 1, 6, 9, 10, 8, 7, 5]
     expect(bubbleSort(nums)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+  })
+})
+
+describe('selectionSort', () => {
+  it('should return sorted array', () => {
+    const nums = [2, 3, 0, 4, 1, 6, 9, 10, 8, 7, 5]
+    expect(selectionSort(nums)).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
   })
 })
--- problems/sort/sorter/solution.js
+++ problems/sort/sorter/solution.js
@@ -1,3 +1,5 @@
+// 冒泡排序
+// 时间复杂度：O(n^2)
 function bubbleSort(nums) {
   for (let i = 0; i < nums.length; i++) {
     for (let j = 0; j < nums.length - i; j++) {
@@ -11,6 +13,24 @@ function bubbleSort(nums) {
   return nums
 }
 
+// 选择排序
+// 时间复杂度：O(n^2)
+function selectionSort(nums) {
+  for (let j = nums.length - 1; j >= 0; j--) {
+    let maxIndex = 0
+    for (let i = 1; i <= j; i++) {
+      if (nums[i] > nums[maxIndex]) {
+        maxIndex = i
+      }
+    }
+    let temp = nums[maxIndex]
+    nums[maxIndex] = nums[j]
+    nums[j] = temp
+  }
+  return nums
+}
+
 module.exports = {
   bubbleSort,
+  selectionSort,
 }
