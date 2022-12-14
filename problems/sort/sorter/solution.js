2082c5bb68c7aeb4ed70b2570c1eab560d4e574b
tkiddo <justforgit@163.com>
2022-12-14 09:42:15 +0800

    problem: update insertSort

--- problems/sort/sorter/solution.js
+++ problems/sort/sorter/solution.js
@@ -93,18 +93,11 @@ function insertSort(nums) {
   for (let i = 1; i < nums.length; i++) {
     const current = nums[i]
     let j = i - 1
-    while (j >= 0) {
-      if (current < nums[j]) {
-        nums[j + 1] = nums[j]
-      } else {
-        nums[j + 1] = current
-        break
-      }
+    while (j >= 0 && current < nums[j]) {
+      nums[j + 1] = nums[j]
       j--
     }
-    if (j < 0) {
-      nums[j + 1] = current
-    }
+    nums[j + 1] = current
   }
   return nums
 }
