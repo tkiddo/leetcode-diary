b8351196a1a78b6a51d044b7c4990909e9d2bdb5
tkiddo <justforgit@163.com>
2023-01-04 17:12:59 +0800

    problem: minNumber solution

--- problems/array/minNumber-middle/solution.js
+++ problems/array/minNumber-middle/solution.js
@@ -2,7 +2,21 @@
  * @param {number[]} nums
  * @return {string}
  */
-const minNumber = function (nums) {}
+const minNumber = function (nums) {
+  const compare = (a, b) => {
+    const s1 = a + '' + b
+    const s2 = b + '' + a
+    if (s1 < s2) {
+      return -1
+    } else if (s1 > s2) {
+      return 1
+    } else {
+      return 0
+    }
+  }
+  const arr = nums.sort(compare)
+  return arr.join('')
+}
 
 module.exports = {
   minNumber,
