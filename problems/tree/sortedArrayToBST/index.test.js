451e13a52127de44fe2524e0a229dd4f6231883c
tkiddo <justforgit@163.com>
2023-01-12 13:56:04 +0800

    problem:sortedArrayToBST solution

--- problems/tree/sortedArrayToBST/index.test.js
+++ problems/tree/sortedArrayToBST/index.test.js
@@ -2,13 +2,12 @@ const { TreeNode, sortedArrayToBST } = require('./solution')
 
 describe('sortedArrayToBST', () => {
   it('should return a tree', () => {
-    const tree = sortedArrayToBST([-10, -3, 0, 5, 9])
-    expect(tree).toEqual(
-      new TreeNode(
-        0,
-        new TreeNode(-3, new TreeNode(-10)),
-        new TreeNode(9, new TreeNode(5))
-      )
-    )
+    const nums = [-10, -3, 0, 5, 9]
+    const root = new TreeNode(0)
+    root.left = new TreeNode(-3)
+    root.left.left = new TreeNode(-10)
+    root.right = new TreeNode(9)
+    root.right.left = new TreeNode(5)
+    expect(sortedArrayToBST(nums)).toEqual(root)
   })
 })
--- problems/tree/sortedArrayToBST/solution.js
+++ problems/tree/sortedArrayToBST/solution.js
@@ -15,7 +15,19 @@ function TreeNode(val, left, right) {
  * @param {number[]} nums
  * @return {TreeNode}
  */
-const sortedArrayToBST = function (nums) {}
+const sortedArrayToBST = function (nums) {
+  function dfs(start, end) {
+    if (start > end) {
+      return null
+    }
+    const mid = Math.floor((start + end) / 2)
+    const node = new TreeNode(nums[mid])
+    node.left = dfs(start, mid - 1)
+    node.right = dfs(mid + 1, end)
+    return node
+  }
+  return dfs(0, nums.length - 1)
+}
 
 module.exports = {
   TreeNode,
