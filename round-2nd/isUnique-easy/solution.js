3e2039955b3fb83028f6bbb773034b6ecd11b721
tkiddo <justforgit@163.com>
2023-10-16 10:01:13 +0800

    2nd: isUnique

--- round-2nd/isUnique-easy/solution.js
+++ round-2nd/isUnique-easy/solution.js
@@ -2,13 +2,27 @@
  * @param {string} astr
  * @return {boolean}
  */
+// const isUnique = function (astr) {
+//   const letter = {}
+//   for(let i =0;i<astr.length;i++){
+//     if(letter[astr[i]]){
+//       return false
+//     }else{
+//       letter[astr[i]] = true
+//     }
+//   }
+//   return true
+// }
+
+// 位运算解法
 const isUnique = function (astr) {
-  const letter = {}
-  for(let i =0;i<astr.length;i++){
-    if(letter[astr[i]]){
+  let mark = 0
+  for (let i = 0; i < astr.length; i++) {
+    const moveBit = astr.charCodeAt(i) - 'a'.charCodeAt(0)
+    if ((mark & (1 << moveBit)) !== 0) {
       return false
-    }else{
-      letter[astr[i]] = true
+    } else {
+      mark |= 1 << moveBit
     }
   }
   return true
